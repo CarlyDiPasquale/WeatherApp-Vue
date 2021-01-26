0bbcc6cb1ff209cf040673756d0273168227e8b4
Carly <dipasqualecarly@gmail.com>
2021-01-25 20:43:55 -0500

    initalizing repository

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="app">
+		hello
+	</div>
+</template>
+
+<script>
+export default {
+	name: "app",
+	data() {
+		return {
+			api_key: "37724586d97558f1bce9ba44c714d622"
+		};
+	}
+};
+</script>
+
+<style></style>
