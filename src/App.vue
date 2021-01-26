a6376ac2fe0135d3b53db38e82ba9cc0ded8d277
Carly <dipasqualecarly@gmail.com>
2021-01-25 21:52:07 -0500

    feat: added bg images

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,50 @@
 <template>
 	<div id="app">
-		hello
+		<main>
+			<div class="search-box">
+				<input type="text" class="search-bar" placeholder="Search..." />
+			</div>
+		</main>
 	</div>
 </template>
 
 <script>
-export default {
-	name: "app",
-	data() {
-		return {
-			api_key: "37724586d97558f1bce9ba44c714d622"
-		};
-	}
-};
+	export default {
+		name: 'app',
+		data() {
+			return {
+				api_key: '37724586d97558f1bce9ba44c714d622',
+			};
+		},
+	};
 </script>
 
-<style></style>
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	body {
+		font-family: 'montserrat', sans-serif;
+	}
+
+	#app {
+		background-image: url('./assets/cold-bg.jpg');
+		background-size: cover;
+		background-position: bottom;
+		transition: 0.4s;
+	}
+
+	main {
+		min-height: 100vh;
+		padding: 25px;
+
+		background-image: linear-gradient(
+			to bottom,
+			rgba(0, 0, 0, 0.25),
+			rgba(0, 0, 0, 0.75)
+		);
+	}
+</style>
